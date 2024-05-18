988c03218dd0f9e6f606fcb9bb35d237f95b2d72
RaniaKaz <xmyst.kaz@gmail.com>
2024-05-18 16:04:27 +0300

    8th commit

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="login.css">
+    <link rel="stylesheet" type="text/css" href="ortak.css">
     <link rel="stylesheet" href="bootstrap-4.0.0-dist (1)/css/bootstrap.css">
     <title>Rania's Website</title>
     <title>Login</title>
@@ -22,32 +23,38 @@
         </div>
         <nav>
             <div class="row">
-                <div class="col-xs-6 col-xl-1">
-                    <a href="Hakkimda.html"> <p class="secenekler">Hakkımda</p></a>
-                </div>
-                <div class="col-xs-6 col-xl-1 ">
-                    <a href="şehrim.html"><p class="secenekler">Şehrim</p></a>
-                </div>
-                <div class="col-xs-6 col-xl-1">
-                    <a href="mirasimiz.html" ><p class="secenekler">mirasımız</p></a> 
-                </div>
-                <div class="col-xs-6 col-xl-1">
-                    <a href="ilgiliAlan.html"><p class="secenekler">İlgili Alanlarım</p></a>
-                </div>
-                <div class="col-xs-6 col-xl-1">
-                    <a href="CV.html"><p class="secenekler">CV</p></a>
-                </div>
-                <div class="col-xs-6 col-xl-1">
-                    <a href="İletişim.html" ><p class="secenekler">İletişim</p></a> 
-                </div>
-                <div class="col-xs-6 col-xl-1">
-                    <a href="login.html" ><p class="secenekler">Log in</p></a> 
-                </div>
-                <div class="col-6 col-xl-5 d-none d-sm-block">
-                    <a href="https://en.wikipedia.org/wiki/Computer_engineering" target="_blank">
-                        <img src="icons/girl.png" id="kiz" class="d-none d-sm-inline">
-                    </a>
-                </div>
+            <style>
+                a:hover .secenekler {
+                    color: lightblue;
+                    color: rgb(102, 21, 21);
+                }
+            </style>
+            <div class="col-xs-6 col-xl-1">
+                <a href="Hakkimda.html" style="color: wheat; text-decoration: none; "> <p class="secenekler">Hakkımda</p></a>
+            </div>
+            <div class="col-xs-6 col-xl-1 ">
+                <a href="şehrim.html"style="color: wheat; text-decoration: none; "><p class="secenekler">Şehrim</p></a>
+            </div>
+            <div class="col-xs-6 col-xl-1">
+                <a href="mirasimiz.html" style="color: wheat; text-decoration: none; "><p class="secenekler">mirasımız</p></a> 
+            </div>
+            <div class="col-xs-6 col-xl-1">
+                <a href="ilgiliAlan.html"style="color: wheat; text-decoration: none; "><p class="secenekler">İlgili Alanlarım</p></a>
+            </div>
+            <div class="col-xs-6 col-xl-1">
+                <a href="CV.html" style="color: wheat; text-decoration: none; "><p class="secenekler">CV</p></a>
+            </div>
+            <div class="col-xs-6 col-xl-1">
+                <a href="İletişim.html" style="color: wheat; text-decoration: none; "><p class="secenekler">İletişim</p></a> 
+            </div>
+            <div class="col-xs-6 col-xl-1">
+                <a href="login.php" style="color: wheat; text-decoration: none; "><p class="secenekler">Log in</p></a> 
+            </div>
+            <div class="col-6 col-xl-5 d-none d-sm-block">
+                <a href="https://en.wikipedia.org/wiki/Computer_engineering" target="_blank">
+                    <img src="icons/girl.png" id="kiz" class="d-none d-sm-inline">
+                </a>
+            </div>
             </div>
         </nav>
         <main>
